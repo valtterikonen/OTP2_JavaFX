8e24b9c51aab2f66abdd41ee39916cdf67fee557
=) <valtteri.ikonen@metropolia.fi>
2024-10-24 10:05:15 +0300

    nauku

--- TemplateMethod/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dicegame game = new Dicegame(20); // Target score to win is 20
-        game.play(3); // Start the game with 3 players
-    }
-}
