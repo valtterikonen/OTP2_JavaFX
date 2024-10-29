8145d78659a3598ed7bf929f9d86a9f5d5c2b3cf
=) <valtteri.ikonen@metropolia.fi>
2024-10-29 11:23:19 +0200

    rm

--- OTP2_JavaFX-1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.otp2_javafx1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.otp2_javafx1 to javafx.fxml;
-    exports com.example.otp2_javafx1;
-}
